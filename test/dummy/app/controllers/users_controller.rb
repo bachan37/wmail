fc72ba90f0fb69ac1b790988a356245a8e203041
Bachan Smruty <bachan37@gmail.com>
2012-08-23 12:46:14 +0530

    adding users model migration and controller-views in dummy

---
+++ test/dummy/app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  # showing login
+  def index
+
+  end
+
+  # showing registration
+  def new
+
+  end
+
+  # make registration process
+  def create
+
+  end
+
+  # make logout
+  def destroy
+
+  end
+end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120822135537) do
+ActiveRecord::Schema.define(:version => 20120823070754) do
+
+  create_table "users", :force => true do |t|
+    t.string   "user_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "wmail_wmail_accounts", :force => true do |t|
     t.integer  "user_id"
