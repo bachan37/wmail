dd642e3cf8deb6306e97ded9152705be16f92159
Manoj Monga <manojm@mindfiresolutions.com>
2012-08-24 16:20:37 +0530

    Modified new account action and view

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   mount Wmail::Engine => "/wmail"
-
+  root :to => 'users#index'
   resources :users, :only => [:index, :new, :create] do
     collection do
       post :signin
